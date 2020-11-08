da0e977381545f1bfc8efde8fe8aeda6da00815b
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-11-08 20:17:35 -0300

    Projeto Criado

---
+++ src/test/java/com/cursoMc/SpringFast/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoMc.SpringFast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
