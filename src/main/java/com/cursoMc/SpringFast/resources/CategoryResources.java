d9cf33b3d2d91a1048376733b1b82a43349e6c16
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-11-08 21:47:35 -0300

    Testando a Classe de Dominio Categoria - Category

--- src/main/java/com/cursoMc/SpringFast/resources/CategoryResources.java
+++ src/main/java/com/cursoMc/SpringFast/resources/CategoryResources.java
@@ -1,8 +1,13 @@
 package com.cursoMc.SpringFast.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursoMc.SpringFast.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -10,9 +15,18 @@ public class CategoryResources {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Category> listar() {
 		
-		return "REST is working";
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 	}
 	
 }
