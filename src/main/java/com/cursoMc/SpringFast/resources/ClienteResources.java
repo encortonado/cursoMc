b07bfcc21aa8943e10cbdf2afb68e46b0f9a0b58
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-11-10 12:50:01 -0300

    Insert Cliente com POST

--- src/main/java/com/cursoMc/SpringFast/resources/ClienteResources.java
+++ src/main/java/com/cursoMc/SpringFast/resources/ClienteResources.java
@@ -1,5 +1,6 @@
 package com.cursoMc.SpringFast.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursoMc.SpringFast.domain.Cliente;
 import com.cursoMc.SpringFast.dto.ClienteDTO;
+import com.cursoMc.SpringFast.dto.ClienteNewDTO;
 import com.cursoMc.SpringFast.services.ClientService;
 
 @RestController
@@ -35,6 +38,16 @@ public class ClienteResources {
 
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		// cria uma forma de passar o id para a url e sendo através de uma boa pratica
+		// de uso, além de inserir dado na tabela
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
@@ -55,22 +68,21 @@ public class ClienteResources {
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 
 		List<Cliente> list = service.findAll();
-		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); 
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page, 
-			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
-			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
-		Page<ClienteDTO> pageDTO = list.map(obj -> new ClienteDTO(obj)); 
+		Page<ClienteDTO> pageDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(pageDTO);
 
 	}
 
-	
 }
--- src/main/java/com/cursoMc/SpringFast/services/ClientService.java
+++ src/main/java/com/cursoMc/SpringFast/services/ClientService.java
@@ -3,6 +3,8 @@ package com.cursoMc.SpringFast.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.cursoMc.SpringFast.domain.City;
 import com.cursoMc.SpringFast.domain.Cliente;
+import com.cursoMc.SpringFast.domain.Endereco;
+import com.cursoMc.SpringFast.domain.enums.TipoCliente;
 import com.cursoMc.SpringFast.dto.ClienteDTO;
+import com.cursoMc.SpringFast.dto.ClienteNewDTO;
 import com.cursoMc.SpringFast.repositories.ClienteRepository;
+import com.cursoMc.SpringFast.repositories.EnderecoRepository;
 import com.cursoMc.SpringFast.services.exceptions.DataIntegrityException;
 import com.cursoMc.SpringFast.services.exceptions.ObjectNotFoundException;
 
@@ -21,60 +28,82 @@ public class ClientService {
 
 	@Autowired
 	private ClienteRepository repos;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 
 		Optional<Cliente> obj = repos.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
-	
-	
 
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repos.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+		
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repos.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repos.deleteById(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
-			
+
 			throw new DataIntegrityException("Não é possivel excluir um cliente cujo está com pedidos na cesta.");
-			
+
 		}
 	}
-	
+
 	public List<Cliente> findAll() {
 		return repos.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+
 		return repos.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDTO) {
-		
+
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 
-	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		City cid = new City(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+
 	
-
 }
-
 
 
 
